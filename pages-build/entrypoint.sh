705ad9b4cbd2cefcb5d3294c589d5ae596477ce9
Xavier Salazar <x.salazar1729@gmail.com>
2020-05-16 16:14:03 -0700

    Initial commit

---
+++ pages-build/entrypoint.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+username="$INPUT_USERNAME"
+token="$INPUT_GITHUB_API_TOKEN"
+
+curl -u $username:$token -X POST https://api.github.com/repos/$username/$username.github.io/pages/builds
